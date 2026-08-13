1bcbab0e655a8d97974b98aac4443a8b5b039430
persipica <94616927+persipica@users.noreply.github.com>
2026-08-13 19:03:55 +0900

    backend 병합

--- src/main/java/com/log/MeetupLog/domain/user/entity/AccountType.java
+++
@@ -1,7 +0,0 @@
-package com.log.MeetupLog.domain.user.entity;
-
-public enum AccountType {
-    MEMBER, // 정식 회원
-    SOCIAL, // 소셜 회원(Kakao 로그인)
-    GUEST   // 게스트
-}
--- src/main/java/com/log/MeetupLog/domain/user/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.log.MeetupLog.domain.user.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
